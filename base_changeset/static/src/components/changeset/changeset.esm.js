ab74bd5054f82174124af805f62330791d9a0c89
Mark Schuit <mark@gig.solutions>
2023-07-10 17:03:22 +0800

    [MIG] First attempt to update JS changeset.esm.js

--- base_changeset/static/src/components/changeset/changeset.esm.js
+++ base_changeset/static/src/components/changeset/changeset.esm.js
@@ -1,38 +1,39 @@
 /** @odoo-module **/
 
-import {qweb} from "web.core";
-import BasicModel from "web.BasicModel";
 import {FormController} from "@web/views/form/form_controller";
 import {FormRenderer} from "@web/views/form/form_renderer";
+import {Model} from "@web/views/model";
+import {patch} from "@web/core/utils/patch";
+import {Popover} from "@web/core/popover/popover";
+import {qweb} from "web.core";
 
-export class ChangeSetBasicModel extends BasicModel {
+patch(Model.prototype, "base_changeset.Model", {
     applyChange(id) {
-        return this._rpc({
-            model: "record.changeset.change",
-            method: "apply",
-            args: [[id]],
-            context: _.extend({}, this.context, {set_change_by_ui: true}),
-        });
-    }
+        return this.orm.call(
+            "record.changeset.change",
+            "apply",
+            [[id]],
+            _.extend({}, this.context, {set_change_by_ui: true})
+        );
+    },
 
     rejectChange(id) {
-        return this._rpc({
-            model: "record.changeset.change",
-            method: "cancel",
-            args: [[id]],
-            context: _.extend({}, this.context, {set_change_by_ui: true}),
-        });
-    }
+        return this.orm.call(
+            "record.changeset.change",
+            "cancel",
+            [[id]],
+            _.extend({}, this.context, {set_change_by_ui: true})
+        );
+    },
 
     getChangeset(modelName, resId) {
         var self = this;
         return new Promise(function (resolve) {
-            return self
-                ._rpc({
-                    model: "record.changeset.change",
-                    method: "get_fields_changeset_changes",
-                    args: [modelName, resId],
-                })
+            return self.orm
+                .call("record.changeset.change", "get_fields_changeset_changes", [
+                    modelName,
+                    resId,
+                ])
                 .then(function (changeset) {
                     var res = {};
                     _.each(changeset, function (changesetChange) {
@@ -44,93 +45,106 @@ export class ChangeSetBasicModel extends BasicModel {
                     resolve(res);
                 });
         });
-    }
-}
+    },
+});
 
-export class ChangeSetFormController extends FormController {
-    start() {
-        return this._super
-            .apply(this, arguments)
-            .then(this._updateChangeset.bind(this));
-    }
+patch(FormController.prototype, "base_changeset.FormController", {
+    setup() {
+        var self = this;
+        this._super.apply(this, arguments);
+        this._updateChangeset.bind(self);
+    },
 
-    update() {
+    // eslint-disable-next-line no-unused-vars
+    saveButtonClicked(params = {}) {
         var self = this;
         var res = this._super.apply(this, arguments);
         res.then(function () {
             self._updateChangeset();
         });
         return res;
-    }
+    },
 
-    _updateChangeset() {
+    beforeLeave() {
         var self = this;
-        var state = this.model.get(this.handle);
-        this.model.getChangeset(state.model, state.data.id).then(function (changeset) {
-            self.renderer.renderChangesetPopovers(changeset);
+        var res = this._super.apply(this, arguments);
+        res.then(function () {
+            self._updateChangeset();
         });
-    }
+        return res;
+    },
+
+    beforeUnload(ev) {
+        var self = this;
+        var res = this._super.apply(this, arguments);
+        res.then(function () {
+            self._updateChangeset(ev.handle);
+        });
+        return res;
+    },
+
+    // eslint-disable-next-line no-unused-vars
+    _updateChangeset(handle) {
+        var self = this;
+        this.model
+            .getChangeset(this.props.resModel, this.props.resId)
+            .then(function (changeset) {
+                self.props.Renderer.prototype.renderChangesetPopovers(changeset);
+            });
+    },
 
     applyChange(id) {
         this.model.applyChange(id).then(this.reload.bind(this));
-    }
+    },
 
     rejectChange(id) {
         this.model.rejectChange(id).then(this.reload.bind(this));
-    }
-}
+    },
+});
 
-export class ChangeSetFormRenderer extends FormRenderer {
+patch(FormRenderer.prototype, "base_changeset.FormRenderer", {
     renderChangesetPopovers(changeset) {
         var self = this;
         _.each(changeset, function (changes, fieldName) {
-            var labelId = self._getIDForLabel(fieldName);
-            var $label = self.$el.find(_.str.sprintf('label[for="%s"]', labelId));
-            if (!$label.length) {
-                var widgets = _.filter(
-                    self.allFieldWidgets[self.state.id],
-                    function (widget) {
-                        return widget.name === fieldName;
-                    }
-                );
-                if (widgets.length === 1) {
-                    var widget = widgets[0];
-                    $label = widget.$el;
-                } else {
-                    return;
-                }
-            }
-            self._renderChangesetPopover($label, changes);
+            var label = document.querySelector(`label[for=${CSS.escape(fieldName)}]`);
+            // If (!$label.length) {
+            //     $label = $.find(_.str.sprintf('field[name="%s"]'), fieldName)
+            //     if (!$label.length)
+            //         return;
+            // }
+            self._renderChangesetPopover(label, changes);
         });
-    }
+    },
 
-    _renderChangesetPopover($el, changes) {
+    htmlToElement(html) {
+        var template = document.createElement("template");
+        template.innerHTML = html;
+        return template.content.firstChild;
+    },
+
+    _renderChangesetPopover(label, changes) {
         var self = this;
-        if (this.mode !== "readonly") {
-            return;
-        }
-        var $button = $(
-            qweb.render("ChangesetButton", {
-                count: changes.length,
-            })
+        var button = this.htmlToElement(
+            qweb.render("ChangesetButton", {count: changes.length})
         );
-
-        $el.append($button);
+        label.append(button);
 
         var options = {
             content: function () {
-                var $content = $(
-                    qweb.render("ChangesetPopover", {
-                        changes: changes,
-                    })
-                );
-                $content.find(".base_changeset_apply").on("click", function () {
-                    self._applyClicked($(this));
-                });
-                $content.find(".base_changeset_reject").on("click", function () {
-                    self._rejectClicked($(this));
-                });
-                return $content;
+                var content = qweb
+                    .render("ChangesetPopover", {changes: changes})
+                    .trim();
+                content
+                    .querySelector(["base_changeset_apply"])
+                    .on("click", function () {
+                        self._applyClicked(this.data("id"));
+                    });
+                content
+                    .querySelector(["base_changeset_reject"])
+                    .on("click", function () {
+                        self._rejectClicked(this.data("id"));
+                    });
+                return content;
             },
             html: true,
             placement: "bottom",
@@ -140,16 +154,17 @@ export class ChangeSetFormRenderer extends FormRenderer {
             template: qweb.render("ChangesetTemplate"),
         };
 
-        $button.popover(options);
-    }
+        var popover = new Popover(options);
+        label.append(popover);
+    },
 
-    _applyClicked($el) {
-        var id = parseInt($el.data("id"), 10);
+    _applyClicked: function (rawId) {
+        var id = parseInt(rawId, 10);
         this.getParent().applyChange(id);
-    }
+    },
 
-    _rejectClicked($el) {
-        var id = parseInt($el.data("id"), 10);
+    _rejectClicked: function (rawId) {
+        var id = parseInt(rawId, 10);
         this.getParent().rejectChange(id);
-    }
-}
+    },
+});
