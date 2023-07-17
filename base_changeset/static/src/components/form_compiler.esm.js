5aad832effc5e51ff67ac79c075d32893efcb6ab
Stefan Rijnhart <stefan@opener.am>
2023-07-17 15:39:36 +0200

    [REF] append nodes in second pass

--- base_changeset/static/src/components/form_compiler.esm.js
+++ base_changeset/static/src/components/form_compiler.esm.js
@@ -2,6 +2,7 @@
 
 import {createElement, getTag} from "@web/core/utils/xml";
 import {FormCompiler} from "@web/views/form/form_compiler";
+import {copyAttributes} from "@web/views/view_compiler";
 import {patch} from "@web/core/utils/patch";
 
 patch(FormCompiler.prototype, "base_changeset.FormCompiler", {
@@ -10,36 +11,51 @@ patch(FormCompiler.prototype, "base_changeset.FormCompiler", {
         this.insertedChangesetButtons = {};
         this.fieldsWithLabel = [];
     },
-    compileField(el, params) {
-        let nodes = this._super.apply(this, arguments);
-        nodes = Array.isArray(nodes) ? nodes : [nodes];
-        const inInnerGroup = getTag(el.parentNode) === "group" && getTag(el.parentNode.parentNode) === "group";
-        const hasLabel = (
-            el.nodeType !== Node.TEXT_NODE
-                && inInnerGroup
-                && (
-                    el.hasAttribute("nolabel")
-                        ? el.getAttribute("nolabel") !== "1"
-                        : true
-                )
-        );
-        if (!hasLabel) {
-            /*
-              Append a changeset button node after the field node.
-              If a separate label node is
-              encountered, the node needs to be removed (TODO).
-            */
-            const fieldName = el.getAttribute("name");
-            const button = createElement("BaseChangesetButton", {
-                record: `props.record`,
-                fieldName: fieldName,
-            });
-            // Store the button in a registry so we can remove it if needed.
-            this.insertedChangesetButtons[fieldName] = button;
-
-            console.log("Attach to field: " + el.getAttribute("name"));
-            nodes.push(button);
+    appendChangesetButtons(el, params) {
+        for (const child of el.children) {
+            console.log(getTag(child));
+            if (getTag(child) === "Field") {
+                // FIXME do not carry group tag at this point
+                const inInnerGroup =
+                    getTag(child.parentNode) === "group" &&
+                    getTag(child.parentNode.parentNode) === "group";
+                const hasLabel =
+                    child.nodeType !== Node.TEXT_NODE &&
+                    inInnerGroup &&
+                    (child.hasAttribute("nolabel")
+                        ? child.getAttribute("nolabel") !== "1"
+                        : true);
+                if (!hasLabel) {
+                    /*
+                      Append a changeset button node after the field node.
+                      If a separate label node is
+                      encountered, the node needs to be removed (TODO).
+                    */
+                    const fieldName = child.getAttribute("name");
+                    const button = createElement("BaseChangesetButton", {
+                        record: `props.record`,
+                        fieldName: fieldName,
+                    });
+                    button.setAttribute(
+                        "Component",
+                        "constructor.components.FormLabel"
+                    );
+                    button.setAttribute("subType", "'item_component'");
+                    // Store the button in a registry so we can remove it if needed.
+                    const button_node = this.compileNode(button, params);
+                    copyAttributes(child, button_node);
+                    this.insertedChangesetButtons[fieldName] = button;
+                    console.log("Attach to field: " + fieldName);
+                    child.after(button_node);
+                }
+            } else {
+                this.appendChangesetButtons(child);
+            }
         }
-        return nodes;
+    },
+    compile(key, params = {}) {
+        const compiled = this._super.apply(this, arguments);
+        this.appendChangesetButtons(compiled, params);
+        return compiled;
     },
 });
