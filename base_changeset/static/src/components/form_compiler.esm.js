b8d8311b629743a74232ec0e5dd61c9f8552babd
Stefan Rijnhart <stefan@opener.am>
2023-07-17 13:29:25 +0200

    wip but cannot append nodes like this

--- base_changeset/static/src/components/form_compiler.esm.js
+++ base_changeset/static/src/components/form_compiler.esm.js
@@ -1,7 +1,7 @@
 /** @odoo-module **/
 
+import {createElement, getTag} from "@web/core/utils/xml";
 import {FormCompiler} from "@web/views/form/form_compiler";
-import {append, createElement, getTag} from "@web/core/utils/xml";
 import {patch} from "@web/core/utils/patch";
 
 patch(FormCompiler.prototype, "base_changeset.FormCompiler", {
@@ -10,30 +10,36 @@ patch(FormCompiler.prototype, "base_changeset.FormCompiler", {
         this.insertedChangesetButtons = {};
         this.fieldsWithLabel = [];
     },
-    compileNode(node, params = {}, evalInvisible = true) {
-        const res = this._super.apply(this, arguments);
-        if (getTag(node, true) == "field") {
-            if (node.getAttribute("name") == "zip") {
-                console.log("debug me");
-            }
-            const addLabel = node.hasAttribute("nolabel")
-                ? node.getAttribute("nolabel") !== "1"
-                : true;
-            if (!addLabel) {
-                /*
-                   Create a changeset button node. If a separate label node is
-                   encountered, the node needs to be removed (TODO).
-                */
-                const fieldName = node.getAttribute("name");
-                const button = createElement("BaseChangesetButton", {
-                    record: `props.record`,
-                    fieldName: fieldName,
-                });
-                // Store the button in a registry so we can remove it if needed.
-                this.insertedChangesetButtons[fieldName] = button;
-                // Append(
-            }
+    compileField(el, params) {
+        let nodes = this._super.apply(this, arguments);
+        nodes = Array.isArray(nodes) ? nodes : [nodes];
+        const inInnerGroup = getTag(el.parentNode) === "group" && getTag(el.parentNode.parentNode) === "group";
+        const hasLabel = (
+            el.nodeType !== Node.TEXT_NODE
+                && inInnerGroup
+                && (
+                    el.hasAttribute("nolabel")
+                        ? el.getAttribute("nolabel") !== "1"
+                        : true
+                )
+        );
+        if (!hasLabel) {
+            /*
+              Append a changeset button node after the field node.
+              If a separate label node is
+              encountered, the node needs to be removed (TODO).
+            */
+            const fieldName = el.getAttribute("name");
+            const button = createElement("BaseChangesetButton", {
+                record: `props.record`,
+                fieldName: fieldName,
+            });
+            // Store the button in a registry so we can remove it if needed.
+            this.insertedChangesetButtons[fieldName] = button;
+
+            console.log("Attach to field: " + el.getAttribute("name"));
+            nodes.push(button);
         }
-        return res;
+        return nodes;
     },
 });
