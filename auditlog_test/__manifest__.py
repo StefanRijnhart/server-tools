29efa986dd430b940e32b257f04b157bf7ac63b5
Stefan Rijnhart <stefan@opener.amsterdam>
2025-09-15 22:06:41 +0200

    [ADD] auditlog_test

---
+++ auditlog_test/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2015 ABF OSIELL <https://osiell.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Audit Log Tests",
+    "version": "16.0.1.0.0",
+    "author": "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/server-tools",
+    "category": "Tools",
+    "depends": ["account", "auditlog"],
+    "data": [],
+    "application": False,
+    "installable": True,
+}
